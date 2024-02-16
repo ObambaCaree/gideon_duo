01c3c35338150a1a1629b5e13631cbcb9b6180f1
Javi17mod <123207842+Javi17mod@users.noreply.github.com>
2024-02-16 07:42:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,99 @@
-async function main() {
-  try {
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
-      'user-agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-    };
-
-    const { sub } = JSON.parse(
-      Buffer.from(process.env.DUOLINGO_JWT.split('.')[1], 'base64').toString()
-    );
-
-    const { fromLanguage, learningLanguage, xpGains } = await fetch(
-      `https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage,xpGains`,
-      { headers }
-    ).then((response) => response.json());
-
-    if (!xpGains || xpGains.length === 0) {
-      console.error('No se encontraron ganancias de XP.');
-      return;
-    }
-
-    for (let i = 0; i < process.env.LESSONS; i++) {
-      const session = await fetch('https://www.duolingo.com/2017-06-30/sessions', {
-        body: JSON.stringify({
-          challengeTypes: [
-          ],
-          fromLanguage,
-          isFinalLevel: false,
-          isV2: true,
-          juicy: true,
-          learningLanguage,
-          skillId: xpGains[0].skillId, // Acceder al primer elemento de xpGains
-          smartTipsVersion: 2,
-          type: 'SPEAKING_PRACTICE',
-        }),
-        headers,
-        method: 'POST',
-      }).then((response) => response.json());
-
-      const response = await fetch(
-        `https://www.duolingo.com/2017-06-30/sessions/${session.id}`,
-        {
-          body: JSON.stringify({
-            ...session,
-            heartsLeft: 0,
-            startTime: (+new Date() - 60000) / 1000,
-            enableBonusPoints: false,
-            endTime: +new Date() / 1000,
-            failed: false,
-            maxInLessonStreak: 9,
-            shouldLearnThings: true,
-          }),
-          headers,
-          method: 'PUT',
-        }
-      ).then((response) => response.json());
-
-      console.log({ xp: response.xpGain });
-    }
-  } catch (error) {
-    console.error('Se produjo un error:', error);
-  }
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
+  'user-agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 }
+var lessonsdone = 0;
+const { sub } = JSON.parse(
+  Buffer.from(process.env.DUOLINGO_JWT.split('.')[1], 'base64').toString(),
+)
 
-main();
+const { fromLanguage, learningLanguage, xpGains } = await fetch(
+  `https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage,xpGains`,
+  {
+    headers,
+  },
+).then(response => response.json())
+
+for (let i = 0; i < process.env.LESSONS; i++) {
+  const session = await fetch('https://www.duolingo.com/2017-06-30/sessions', {
+    body: JSON.stringify({
+      challengeTypes: [
+        'assist',
+        'characterIntro',
+        'characterMatch',
+        'characterPuzzle',
+        'characterSelect',
+        'characterTrace',
+        'completeReverseTranslation',
+        'definition',
+        'dialogue',
+        'form',
+        'freeResponse',
+        'gapFill',
+        'judge',
+        'listen',
+        'listenComplete',
+        'listenMatch',
+        'match',
+        'name',
+        'listenComprehension',
+        'listenIsolation',
+        'listenTap',
+        'partialListen',
+        'partialReverseTranslate',
+        'readComprehension',
+        'select',
+        'selectPronunciation',
+        'selectTranscription',
+        'syllableTap',
+        'syllableListenTap',
+        'speak',
+        'tapCloze',
+        'tapClozeTable',
+        'tapComplete',
+        'tapCompleteTable',
+        'tapDescribe',
+        'translate',
+        'typeCloze',
+        'typeClozeTable',
+        'typeCompleteTable',
+      ],
+      fromLanguage,
+      isFinalLevel: false,
+      isV2: true,
+      juicy: true,
+      learningLanguage,
+skillId: xpGains.find(xpGain => xpGain.skillId).skillId,
+  smartTipsVersion: 2,
+      type: 'SPEAKING_PRACTICE',
+    }),
+    headers,
+    method: 'POST',
+  }).then(response => response.json())
+
+  const response = await fetch(
+    `https://www.duolingo.com/2017-06-30/sessions/${session.id}`,
+    {
+      body: JSON.stringify({
+        ...session,
+        heartsLeft: 0,
+        startTime: (+new Date() - 60000) / 1000,
+        enableBonusPoints: false,
+        endTime: +new Date() / 1000,
+        failed: false,
+        maxInLessonStreak: 9,
+        shouldLearnThings: true,
+      }),
+      headers,
+      method: 'PUT',
+    },
+  ).then(response => response.json())
+
+var totalExp = process.env.LESSONS * 20;
+  lessonsdone = lessonsdone + 20;
+  var percentaged = lessonsdone / totalExp;
+  var percentage = percentaged * 100;
+console.warn(totalExp + " / " + lessonsdone + " | " + percentage + "%")
+}
